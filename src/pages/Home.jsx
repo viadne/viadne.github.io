ec77742b2d18ed8ff5086e59757c900b20623063
viadne <hoomin982@gmail.com>
2026-03-12 15:04:47 -0700

    home add sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,9 @@
 import BlogImage from "../components/BlogImage"
+import Sidebar from "../components/SideBar"
 
 const Home = () => {
   return <div>
+    <Sidebar/>
     home
     <div className='underline'>
       SDFKLKDS 
